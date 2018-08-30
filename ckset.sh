b56efd97f5db1480f35aa688bb060c547fe99171
supabassmasta <supabassmasta@outlook.com>
2018-08-30 05:25:57 +0200

    ckset.sh

---
+++ ckset.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#chuckcfg;
+sudo chmod +r /dev/input/event4;
+
+#lsdev;
+ls -l /dev/input/by-path/;ls -l /dev/input;
+
+echo ""
+echo "PRESS ENTER TO CONTINUE"
+echo ""
+read tmp;
+
+qjackctl &
+
+echo ""
+echo "PRESS ENTER TO CONTINUE"
+echo ""
+read tmp;
+
+# to execute commands in child terminal:
+# add following line at the end of .bashrc: eval "$BASH_POST_RC"
+# call term this way: BASH_POST_RC='echo coucou;echo cucu;' gnome-terminal
+
+
+gnome-terminal --working-directory=$PWD/intro_tempura/ --geometry 118x82+0+0
+gnome-terminal --working-directory=$PWD/ayawaska/ --geometry 118x82+0+0
+gnome-terminal --working-directory=$PWD/indian_kid/ --geometry 118x82-0+0
+gnome-terminal --working-directory=$PWD/costa_dub/ --geometry 118x82+0+0
+gnome-terminal --working-directory=$PWD/ederlezi/ --geometry 118x82-0+0
+gnome-terminal --working-directory=$PWD/mantra/ --geometry 118x82+0+0
