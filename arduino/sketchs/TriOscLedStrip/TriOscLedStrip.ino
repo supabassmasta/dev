6cad78d9c66262af6ca32676fef85968c3d83486
supabassmasta <supabassmasta@outlook.com>
2018-11-12 16:52:19 +0100

    offset problem make the tri oscillate right left

--- arduino/sketchs/TriOscLedStrip/TriOscLedStrip.ino
+++ arduino/sketchs/TriOscLedStrip/TriOscLedStrip.ino
@@ -10,8 +10,11 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(300, PIN, NEO_GRB + NEO_KHZ800);
 class Tri {
   public :
   int cnt;
+  bool up = true;
+  bool up_start = true;
 
   int offset_cnt = 0;
+  bool up_offset = true;
 
   int min = 0;
   int max = 255;
@@ -19,7 +22,7 @@ class Tri {
   int period_num = 6;
   int period_den = 1;
 
-  int offset_num = 1;
+  int offset_num = 4;
   int offset_den = 1;
   
   // private TODO Clean
@@ -35,12 +38,24 @@ class Tri {
     sub_cnt_offset ++;
     if (sub_cnt_offset > offset_den) {
       sub_cnt_offset = 0;
-      offset_cnt += offset_num;
      
-      if (offset_cnt > 255) offset_cnt = 0;
+      if (offset_cnt > max) {
+        up_offset = false;
+      }
+       if (offset_cnt < min) {
+        up_offset = true;
+      }
+     
+      if (up_offset ) {
+        offset_cnt += offset_num;
+      }    
+      else {
+        offset_cnt -= offset_num;   
+      }
     }
     cnt = offset_cnt;
     sub_cnt = 0;
+    up = up_start;
     
   }
 
@@ -48,9 +63,22 @@ class Tri {
     sub_cnt ++;
     if (sub_cnt > period_den) {
       sub_cnt = 0;
-      cnt += period_num;
 
-      if (cnt > 255) cnt = 0; // SAWTOOTH, TODO make Tri
+      if (cnt > max) {
+        cnt = max;
+        up = 0;
+      }
+      else if (cnt < min) {
+        cnt = min;
+        up = 1;
+      }
+
+      if (up) {
+        cnt += period_num;
+      }
+      else {
+        cnt -= period_num;
+      }
     }
     
     return cnt;
@@ -60,12 +88,16 @@ class Tri {
 
 };
 
-  Tri t1 = Tri();
+Tri t1 = Tri();
+Tri t2 = Tri();
+Tri t3 = Tri();
 
 void setup() {
   strip.begin();
   strip.setBrightness(32); // Max 255
   strip.show(); // Initialize all pixels to 'off'
+
+  show1_conf();
 
 //  Serial.begin(115200);
 }
@@ -91,19 +123,49 @@ void theaterChase(uint32_t c, uint8_t wait) {
     }
   }
 }
+void show1_conf() {
+  t1.offset_cnt = 0;
+  t1.min = -255;
+  t1.max = 255;
+  t1.period_num = 4 * 16;
+  t1.period_den = 1;
+  t1.offset_num = 8 * 8;
+  t1.offset_den = 1;
+  t1.up_start = true;
+
+  t2.offset_cnt = 0;
+  t2.min = -64;
+  t2.max = 64;
+  t2.period_num = 7;
+  t2.period_den = 0;
+  t2.offset_num = 1;
+  t2.offset_den = 1;
+  t2.up_start = true;
+
+}
+
 
 void show1() {
       t1.new_show();
+      t2.new_show();
 
       for (uint16_t i=0; i < strip.numPixels(); i++) {
           int g;
+          int b;
+          int r;
 
           g = t1.process();
-// Serial.println(g);
-//      delay (200);
+//          b = t2.process();
+//          r = g*b ;
+
           if (g<0) g = 0;
           if (g>255) g = 255;
-          strip.setPixelColor(i, strip.Color(0, g, 0));
+          if (b<0) b = 0;
+          if (b>255) b = 255;
+         if (r<0) r = 0;
+          if (r>255) r = 255;
+
+          strip.setPixelColor(i, strip.Color(r, g, b));
       }
 
 //      Serial.println("SHOW");
