d597f20a489657c68500f77adc2fc70bd0f20052
supabassmasta <supabassmasta@outlook.com>
2018-11-20 18:25:42 +0100

    reduce arduino led error duration

--- arduino/sketchs/TriOscLedStrip/TriOscLedStrip.ino
+++ arduino/sketchs/TriOscLedStrip/TriOscLedStrip.ino
@@ -108,7 +108,7 @@ void setup() {
 
 void error() {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(100);                       // wait for a second
+    delay(10);                       // wait for a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 }
 
