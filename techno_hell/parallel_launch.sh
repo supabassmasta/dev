0d932d22b62f33370c7e8f02e7d7e805865409fb
supabassmasta <supabassmasta@outlook.com>
2020-01-09 11:28:31 +0100

    techno hell

---
+++ techno_hell/parallel_launch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASH_POST_RC='cd ../techno_hell_aux1; ctopj;' gnome-terminal
+BASH_POST_RC='cd ../techno_hell_aux2; ctopj;' gnome-terminal
+BASH_POST_RC='cd ../techno_hell_aux3; ctopj;' gnome-terminal
+
+chuckj song_top.ck --bufsize4096 --srate:44100
